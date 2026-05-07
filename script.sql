8acf58a76c58ac846844d0dcbc6b0101791e0583
Isadora Maria <isadorabezerra.barros@gmail.com>
2026-05-07 20:14:56 -0300

    Criação de script sql

---
+++ script.sql
@@ -0,0 +1,11 @@
+-- criar detabase `escola_db`
+create DATABASE 'escola_db';
+
+-- criar tabela `tb_alunos`
+create table 'tb_alunos' (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(19) NOT NULL,
+    Ttelefone VARCHAR(14);
+);
